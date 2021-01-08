5869da772d62ad46d294572bad7935363030cbda
granttsai <grantjlt@gmail.com>
2021-01-08 11:46:09 +0800

    Create j.js

---
+++ site/static/j.js
@@ -0,0 +1,24 @@
+var g=getRandom(1,5);
+if(g==1)
+{
+document.write("<a href=https://m.taphy.com/2021/01/best.html><img src=https://1.bp.blogspot.com/-4HXKaYuylQU/X_UnOG0GzQI/AAAAAAAAIL0/MR80QHzaMkYeIJ2WFXzu2Zs6LrOJKMs6gCLcBGAsYHQ/s0/j.png></a>");
+}
+if(g==2)
+{
+document.write("<a href=https://m.taphy.com/2021/01/best.html><img src=https://1.bp.blogspot.com/-4HXKaYuylQU/X_UnOG0GzQI/AAAAAAAAIL0/MR80QHzaMkYeIJ2WFXzu2Zs6LrOJKMs6gCLcBGAsYHQ/s0/j.png></a>");
+}
+if(g==3)
+{
+document.write("<a href=https://m.taphy.com/2021/01/best.html><img src=https://1.bp.blogspot.com/-4HXKaYuylQU/X_UnOG0GzQI/AAAAAAAAIL0/MR80QHzaMkYeIJ2WFXzu2Zs6LrOJKMs6gCLcBGAsYHQ/s0/j.png></a>");
+}
+if(g==4)
+{
+document.write("<a href=https://m.taphy.com/2021/01/best.html><img src=https://1.bp.blogspot.com/-4HXKaYuylQU/X_UnOG0GzQI/AAAAAAAAIL0/MR80QHzaMkYeIJ2WFXzu2Zs6LrOJKMs6gCLcBGAsYHQ/s0/j.png></a>");
+}
+if(g==5)
+{
+document.write("<a href=https://m.taphy.com/2021/01/best.html><img src=https://1.bp.blogspot.com/-4HXKaYuylQU/X_UnOG0GzQI/AAAAAAAAIL0/MR80QHzaMkYeIJ2WFXzu2Zs6LrOJKMs6gCLcBGAsYHQ/s0/j.png></a>");
+}
+function getRandom(min,max){
+    return Math.floor(Math.random()*max)+min;
+};
