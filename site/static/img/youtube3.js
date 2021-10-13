148bf0d0fa427841e118bb2dbc59807578d4196f
granttsai <grantjlt@gmail.com>
2021-10-13 09:12:46 +0800

    Update youtube3.js

--- site/static/img/youtube3.js
+++ site/static/img/youtube3.js
@@ -1,1 +1,27 @@
-document.write("<br><a href='https://jp.hiyawu.com/p/k.html'>聽更多的歌</a><br><a href=k6.html><img src=http://img.youtube.com/vi/Y4nEEZwckuU/0.jpg><br>群青</a><br><a href=k3.html><img src=http://img.youtube.com/vi/Rz23lEKDB18/0.jpg><br>Moonlight Magic花澤香菜</a><br><a href=k5.html><img src=http://img.youtube.com/vi/dy90tA3TT1c/0.jpg><br>YOASOBI怪物</a><br><a href=k2.html><img src=http://img.youtube.com/vi/4DxL6IKmXx4/0.jpg><br>炎</a><br><a href=k4.html><img src=http://img.youtube.com/vi/wJQ9ig_d8yY/0.jpg><br>大正浪漫</a><br><a href=k8.html><img src=http://img.youtube.com/vi/gB3gUq5A1W0/0.jpg><br>境界線</a><br><a href=k9.html><img src=http://img.youtube.com/vi/SX_ViT4Ra7k/0.jpg><br>Lemon</a><br><a href=k10.html><img src=http://img.youtube.com/vi/kzZ6KXDM1RI/0.jpg><br>ドライフラワー 優里</a><br><a href=k11.html><img src=http://img.youtube.com/vi/VyvhvlYvRnc/0.jpg><br>YOASOBI優しい彗星</a><br><a href=k12.html><img src=http://img.youtube.com/vi/ony539T074w/0.jpg><br>King Gnu 白日</a><br><a href=k13.html><img src=http://img.youtube.com/vi/_IkopJwRDKU/0.jpg><br>LiSA紅蓮華</a><br><a href=k15.html><img src=http://img.youtube.com/vi/JGnfRSaEY78/0.jpg><br>貓DISH</a>");
+document.write("<br><a href='https://jp.hiyawu.com/p/k.html'>聽更多的歌</a><br><br>");
+
+document.write("<br><a href=k6.html>YOASOBI群青<br><img src=http://img.youtube.com/vi/Y4nEEZwckuU/0.jpg></a>");
+
+document.write("<br><a href=k3.html>Moonlight Magic花澤香菜<br><img src=http://img.youtube.com/vi/Rz23lEKDB18/0.jpg></a>");
+
+
+document.write("<br><a href=k5.html>YOASOBI怪物<br><img src=http://img.youtube.com/vi/dy90tA3TT1c/0.jpg></a>");
+
+
+document.write("<br><a href=k2.html>LiSA炎<br><img src=http://img.youtube.com/vi/4DxL6IKmXx4/0.jpg></a>");
+
+document.write("<br><a href=k4.html>YOASOBI大正浪漫<br><img src=http://img.youtube.com/vi/wJQ9ig_d8yY/0.jpg></a>");
+
+document.write("<br><a href=k8.html>境界線<br><img src=http://img.youtube.com/vi/gB3gUq5A1W0/0.jpg></a>");
+
+document.write("<br><a href=k9.html>米津玄師Lemon<br><img src=http://img.youtube.com/vi/SX_ViT4Ra7k/0.jpg></a>");
+
+document.write("<br><a href=k10.html>優里ドライフラワー<br><img src=http://img.youtube.com/vi/kzZ6KXDM1RI/0.jpg></a>");
+
+document.write("<br><a href=k11.html>YOASOBI優しい彗星<br><img src=http://img.youtube.com/vi/VyvhvlYvRnc/0.jpg></a>");
+
+document.write("<br><a href=k12.html>King Gnu 白日<br><img src=http://img.youtube.com/vi/ony539T074w/0.jpg></a>");
+
+document.write("<br><a href=k13.html>LiSA紅蓮華<br><img src=http://img.youtube.com/vi/_IkopJwRDKU/0.jpg></a>");
+
+document.write("<br><a href=k15.html>DISH貓<br><img src=http://img.youtube.com/vi/JGnfRSaEY78/0.jpg></a>");
