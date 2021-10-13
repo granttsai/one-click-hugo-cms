74bc0af0067e948384c168a3b4cbfb6ac63f00ef
granttsai <grantjlt@gmail.com>
2021-10-13 14:15:14 +0800

    Update rule.js

--- site/static/img/rule.js
+++ site/static/img/rule.js
@@ -4,10 +4,13 @@ document.write("<br>2.分身");
 document.write("<br>3.沒有禮貌");
 document.write("<br>4.惡意搗亂");
 document.write("<br>5.販賣");
-document.write("<br>6.非關日文學習者");
-document.write("<br>7.強迫推銷用其它語言學習日文");
+document.write("<br>6.非關學習者");
+document.write("<br>7.強迫推銷用其它語言學習");
 document.write("<br>8.侵犯版權");
+document.write("<br>9.長輩圖");
+document.write("<br>10.乞討文");
+
 document.write("<br>也請大家務必遵守版規哦。");
-document.write("<br>有以上問題者一律以版規處理");
+document.write("<br>違反以上問題者一律以版規處理");
 document.write("<br>不另行通知。");
-document.write("<br><br>請確實遵守版規<br>");
+document.write("<br>請務必確實遵守版規<br>");
