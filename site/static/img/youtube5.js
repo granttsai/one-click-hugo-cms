fc390f631baac6d875d042fa4b80428f13b1ecf4
granttsai <grantjlt@gmail.com>
2021-10-13 10:58:37 +0800

    Update youtube5.js

--- site/static/img/youtube5.js
+++ site/static/img/youtube5.js
@@ -1,10 +1,10 @@
 document.write("<br><a href='/p/s.html'>聽更多的歌</a><br>");
 
-document.write("<br><a href=s3.html>BTS (방탄소년단) Life Goes On<br><img src=http://img.youtube.com/vi/-5q5mZbe3V8/0.jpg></a>");
+document.write("<br><a href=s3.html>BTS 防彈少年團 방탄소년단 Butter<br><img src=http://img.youtube.com/vi/YLXfyHsfFz0/0.jpg></a>");
 
 document.write("<br><a href=s2.html>Red Velvet 레드벨벳 Queendom<br><img src=http://img.youtube.com/vi/c9RzZpV460k/0.jpg></a>");
 
-document.write("<br><a href=s5.html>BTS (방탄소년단) Permission to Dance<br><img src=http://img.youtube.com/vi/CuklIb9d3fI/0.jpg></a>");
+document.write("<br><a href=s5.html>BTS 防彈少年團 방탄소년단 Permission to Dance<br><img src=http://img.youtube.com/vi/CuklIb9d3fI/0.jpg></a>");
 
 document.write("<br><a href=s7.html>aespa 에스파 Savage<br><img src=http://img.youtube.com/vi/WPdWvnAAurg/0.jpg></a>");
 
