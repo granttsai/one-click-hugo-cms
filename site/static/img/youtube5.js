80743300719fe9ab8463e34c24e863232696cbd5
granttsai <grantjlt@gmail.com>
2021-10-13 09:17:22 +0800

    Update youtube5.js

--- site/static/img/youtube5.js
+++ site/static/img/youtube5.js
@@ -1,1 +1,15 @@
-document.write("<br><a href='/p/s.html'>聽更多的歌</a><br><br><a href=s2.html>Red Velvet 레드벨벳 'Queendom' <br><img src=http://img.youtube.com/vi/c9RzZpV460k/0.jpg></a><br><a href=s5.html>BTS (방탄소년단) Permission to Dance<br><img src=http://img.youtube.com/vi/CuklIb9d3fI/0.jpg></a><br><a href=s7.html>aespa 에스파 'Savage'<br><img src=http://img.youtube.com/vi/WPdWvnAAurg/0.jpg></a><br><a href=s4.html>KEY 키 'BAD LOVE'<br><img src=http://img.youtube.com/vi/OYWWnd9ACMI/0.jpg></a><br><a href=s7.html>NCT 127 엔시티 127 'Sticker'<br><img src=http://img.youtube.com/vi/1oYWnbTSang/0.jpg></a><br><a href=s8.html>SUNGMIN 성민 'Goodnight, Summer'<br><img src=http://img.youtube.com/vi/xSXslki5xIY/0.jpg></a>");
+document.write("<br><a href='/p/s.html'>聽更多的歌</a><br>");
+
+document.write("<br><a href=s3.html>BTS (방탄소년단) Life Goes On<br><img src=http://img.youtube.com/vi/-5q5mZbe3V8/0.jpg></a>");
+
+document.write("<br><a href=s2.html>Red Velvet 레드벨벳 Queendom<br><img src=http://img.youtube.com/vi/c9RzZpV460k/0.jpg></a>");
+
+document.write("<br><a href=s5.html>BTS (방탄소년단) Permission to Dance<br><img src=http://img.youtube.com/vi/CuklIb9d3fI/0.jpg></a>");
+
+document.write("<br><a href=s7.html>aespa 에스파 Savage<br><img src=http://img.youtube.com/vi/WPdWvnAAurg/0.jpg></a>");
+
+document.write("<br><a href=s4.html>KEY 키 BAD LOVE<br><img src=http://img.youtube.com/vi/OYWWnd9ACMI/0.jpg></a>");
+
+document.write("<br><a href=s6.html>NCT 127 엔시티 127 Sticker<br><img src=http://img.youtube.com/vi/1oYWnbTSang/0.jpg></a>");
+
+document.write("<br><a href=s8.html>SUNGMIN 성민 Goodnight Summer<br><img src=http://img.youtube.com/vi/xSXslki5xIY/0.jpg></a>");
