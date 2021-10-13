8059fc2f64cd2b54568e80cb21f4c7babcc0b799
granttsai <grantjlt@gmail.com>
2021-10-13 13:47:54 +0800

    Update youtube6.js

--- site/static/img/youtube6.js
+++ site/static/img/youtube6.js
@@ -1,33 +1,33 @@
 document.write("<br><a href='/p/s.html'>聽更多的歌</a><br>");
 
 
-document.write("<br><a href=s4.html>Driver License<br><img src=http://img.youtube.com/vi/ZmDBbnmKpqQ/0.jpg></a>");
-document.write("<br><a href=s2.html>BTS Butter<br><img src=http://img.youtube.com/vi/WMweEpGlu_U/0.jpg></a>");
-document.write("<br><a href=s3.html>BTS<br><img src=http://img.youtube.com/vi/CuklIb9d3fI/0.jpg></a>");
+document.write("<br><a href=s4.html>Olivia Rodrigo Driver License<br><img src=http://img.youtube.com/vi/ZmDBbnmKpqQ/0.jpg></a>");
+document.write("<br><a href=s2.html>BTS防彈少年團Butter<br><img src=http://img.youtube.com/vi/WMweEpGlu_U/0.jpg></a>");
+document.write("<br><a href=s3.html>BTS防彈少年團Permission to Dance<br><img src=http://img.youtube.com/vi/CuklIb9d3fI/0.jpg></a>");
 
 
 
 
-document.write("<br><a href=s7.html>aespa 에스파 Savage<br><img src=http://img.youtube.com/vi//0.jpg></a>");
+document.write("<br><a href=s7.html><br><img src=http://img.youtube.com/vi//0.jpg></a>");
 
 
 
-document.write("<br><a href=s6.html>NCT 127 엔시티 127 Sticker<br><img src=http://img.youtube.com/vi//0.jpg></a>");
+document.write("<br><a href=s6.html><br><img src=http://img.youtube.com/vi//0.jpg></a>");
 
-document.write("<br><a href=s8.html>SUNGMIN 성민 Goodnight Summer<br><img src=http://img.youtube.com/vi//0.jpg></a>");
+document.write("<br><a href=s8.html><br><img src=http://img.youtube.com/vi//0.jpg></a>");
 
-document.write("<br><a href=s9.html>KANGTA 강타 아마 Maybe<br><img src=http://img.youtube.com/vi//0.jpg></a>");
+document.write("<br><a href=s9.html><br><img src=http://img.youtube.com/vi//0.jpg></a>");
 
-document.write("<br><a href=s10.html>SUNGMIN 성민 설렘꽃 Blooming<br><img src=http://img.youtube.com/vi//0.jpg></a>");
+document.write("<br><a href=s10.html><br><img src=http://img.youtube.com/vi//0.jpg></a>");
 
-document.write("<br><a href=s11.html>HYO 효연 Second (Feat. 비비 (BIBI))'<br><img src=http://img.youtube.com/vi//0.jpg></a>");
+document.write("<br><a href=s11.html><br><img src=http://img.youtube.com/vi//0.jpg></a>");
 
-document.write("<br><a href=s12.html>KANGTA 강타 7월의 크리스마스 (Christmas in July)<br><img src=http://img.youtube.com/vi//0.jpg></a>");
+document.write("<br><a href=s12.html><br><img src=http://img.youtube.com/vi//0.jpg></a>");
 
-document.write("<br><a href=s13.html>D.O. 디오 Rose<br><img src=http://img.youtube.com/vi//0.jpg></a>");
+document.write("<br><a href=s13.html><br><img src=http://img.youtube.com/vi//0.jpg></a>");
 
-document.write("<br><a href=s14.html>aespa 에스파 Next Level<br><img src=http://img.youtube.com/vi//0.jpg></a>");
+document.write("<br><a href=s14.html><br><img src=http://img.youtube.com/vi//0.jpg></a>");
 
-document.write("<br><a href=s15.html>NCT 127 gimme gimme<br><img src=http://img.youtube.com/vi//0.jpg></a>");
+document.write("<br><a href=s15.html><br><img src=http://img.youtube.com/vi//0.jpg></a>");
 
-document.write("<br><a href=s16.html>BAEKHYUN ベクヒョン Get You Alone<br><img src=http://img.youtube.com/vi//0.jpg></a>");
+document.write("<br><a href=s16.html><br><img src=http://img.youtube.com/vi//0.jpg></a>");
