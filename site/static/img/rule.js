811174966a22f6abf91cf28cc392042ad61184a3
granttsai <grantjlt@gmail.com>
2021-10-13 14:10:44 +0800

    Create rule.js

---
+++ site/static/img/rule.js
@@ -0,0 +1,13 @@
+document.write("<br>請先閱讀以下版規<br>");
+document.write("<br>1.廣告");
+document.write("<br>2.分身");
+document.write("<br>3.沒有禮貌");
+document.write("<br>4.惡意搗亂");
+document.write("<br>5.販賣");
+document.write("<br>6.非關日文學習者");
+document.write("<br>7.強迫推銷用其它語言學習日文");
+document.write("<br>8.侵犯版權");
+document.write("<br>也請大家務必遵守版規哦。");
+document.write("<br>有以上問題者一律以版規處理");
+document.write("<br>不另行通知。");
+document.write("<br><br>請確實遵守版規<br>");
