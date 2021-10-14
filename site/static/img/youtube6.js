3589a1b4edc1e527dae302527501314f03a973cc
granttsai <grantjlt@gmail.com>
2021-10-14 10:35:11 +0800

    Update youtube6.js

--- site/static/img/youtube6.js
+++ site/static/img/youtube6.js
@@ -4,11 +4,7 @@ document.write("<br><a href='/p/s.html'>聽更多的歌</a><br>");
 document.write("<br><a href=s4.html>Olivia Rodrigo Driver License<br><img src=http://img.youtube.com/vi/ZmDBbnmKpqQ/0.jpg></a>");
 document.write("<br><a href=s2.html>BTS防彈少年團Butter<br><img src=http://img.youtube.com/vi/WMweEpGlu_U/0.jpg></a>");
 document.write("<br><a href=s3.html>BTS防彈少年團Permission to Dance<br><img src=http://img.youtube.com/vi/CuklIb9d3fI/0.jpg></a>");
-
-
-
-
-document.write("<br><a href=s7.html><br><img src=http://img.youtube.com/vi//0.jpg></a>");
+document.write("<br><a href=s5.html>The Beatles - Here Comes The Sun<br><img src=http://img.youtube.com/vi/KQetemT1sWc/0.jpg></a>");
 
 
 
