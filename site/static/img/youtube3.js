aae2212c916cfa6a87c5fe83154a3a2339482158
granttsai <grantjlt@gmail.com>
2021-10-18 15:16:41 +0800

    Update youtube3.js

--- site/static/img/youtube3.js
+++ site/static/img/youtube3.js
@@ -99,40 +99,44 @@ document.write("<br><a href=k67.html>I LOVE Official髭男dism<br><img src=http:
 document.write("<br><a href=k68.html>愛にできることはまだあるかい RADWIMPS<br><img src=http://img.youtube.com/vi/EQ94zflNqn4/0.jpg></a>");
 document.write("<br><a href=k69.html>僕が死のうと思ったのは 中島美嘉<br><img src=http://img.youtube.com/vi/HG9t_LbrtnQ/0.jpg></a>");
 
+document.write("<br><a href=k70.html>香水 瑛人<br><img src=http://img.youtube.com/vi/9MjAJSoaoSo/0.jpg></a>");
+document.write("<br><a href=k71.html>雪の華 中島美嘉<br><img src=http://img.youtube.com/vi/oIoaIlPpIcA/0.jpg></a>");
+document.write("<br><a href=k72.html>小さな恋のうた 粉ミルク<br><img src=http://img.youtube.com/vi/_0bb30seo28/0.jpg></a>");
+document.write("<br><a href=k73.html>たぶん YOASOBI<br><img src=http://img.youtube.com/vi/8iuLXODzL04/0.jpg></a>");
+document.write("<br><a href=k74.html>涙そうそう 夏川りみ<br><img src=http://img.youtube.com/vi/ktQlk-llTmE/0.jpg></a>");
+document.write("<br><a href=k75.html>カワキヲアメク 美波<br><img src=http://img.youtube.com/vi/0YF8vecQWYs/0.jpg></a>");
+
+document.write("<br><a href=k76.html>Ref:rain Aimer あいみょん<br><img src=http://img.youtube.com/vi/mvkbCZfwWzA/0.jpg></a>");
+
+
+document.write("<br><a href=k77.html>ようかい体操第一 Dream5<br><img src=http://img.youtube.com/vi/VyKLQXOj0ts/0.jpg></a>");
+document.write("<br><a href=k78.html>ハッピーエンド back number<br><img src=http://img.youtube.com/vi/T8y_RsF4TSw/0.jpg></a>");
+document.write("<br><a href=k79.html>残酷な天使のテーゼ 高橋洋子<br><img src=http://img.youtube.com/vi/vPkOZm-2cNA/0.jpg></a>");
+document.write("<br><a href=k80.html>竈門炭治郎のうた 椎名豪 中川奈美<br><img src=http://img.youtube.com/vi/ViMu9dfe3_o/0.jpg></a>");
+document.write("<br><a href=k81.html>小さな恋のうた MONGOL800<br><img src=http://img.youtube.com/vi/u8EkSB9zSpE/0.jpg></a>");
+document.write("<br><a href=k82.html>名前を呼ぶよ ラックライフ<br><img src=http://img.youtube.com/vi//0.jpg></a>");
+document.write("<br><a href=k83.html>HANABI Mr.Children<br><img src=http://img.youtube.com/vi/EXxaBXKjl6Q/0.jpg></a>");
+document.write("<br><a href=k84.html>恋音と雨空 AAA<br><img src=http://img.youtube.com/vi/A_1t2Dkd2Io/0.jpg></a>");
+document.write("<br><a href=k85.html>明日晴れるかな 桑田佳祐<br><img src=http://img.youtube.com/vi/vjj16qog4vQ/0.jpg></a>");
+document.write("<br><a href=k86.html>うっせぇわ Ado<br><img src=http://img.youtube.com/vi/Qp3b-RXtz4w/0.jpg></a>");
+
+document.write("<br><a href=k87.html>生きていたんだよな Aimer あいみょん<br><img src=http://img.youtube.com/vi/uvav9EmTxrs/0.jpg></a>");
+document.write("<br><a href=k88.html>スパークル RADWIMPS<br><img src=http://img.youtube.com/vi/a2GujJZfXpg/0.jpg></a>");
+document.write("<br><a href=k89.html>THERE IS A REASON 鈴木このみ<br><img src=http://img.youtube.com/vi/EOVQh5xyrKg/0.jpg></a>");
+document.write("<br><a href=k90.html>花に亡霊 ヨルシカ<br><img src=http://img.youtube.com/vi/9lVPAWLWtWc/0.jpg></a>");
+
+document.write("<br><a href=k91.html>Flamingo 米津玄師<br><img src=http://img.youtube.com/vi/Uh6dkL1M9DM/0.jpg></a>");
+document.write("<br><a href=k92.html>千本桜 和楽器バンド<br><img src=http://img.youtube.com/vi/K_xTet06SUo/0.jpg></a>");
+document.write("<br><a href=k93.html>感電 米津玄師<br><img src=http://img.youtube.com/vi/UFQEttrn6CQ/0.jpg></a>");
+document.write("<br><a href=k94.html>春雷 米津玄師<br><img src=http://img.youtube.com/vi/zkNzxsaCunU/0.jpg></a>");
+
+
 
 /*
 document.write("<br><a href=k61.html>僕が死のうと思ったのは amazarashi<br><img src=http://img.youtube.com/vi/4M1amUJWJvA/0.jpg></a>");
 document.write("<br><a href=k7.html>紅蓮の弓矢 - Linked Horizon<br><img src=http://img.youtube.com/vi//0.jpg></a>");
+document.write("<br><a href=k95.html>海の声 桐谷健太<br><img src=http://img.youtube.com/vi//0.jpg></a>");
 
-document.write("<br><a href=k70.html>香水 瑛人<br><img src=http://img.youtube.com/vi//0.jpg></a>");
-document.write("<br><a href=k71.html>雪の華 中島美嘉<br><img src=http://img.youtube.com/vi//0.jpg></a>");
-document.write("<br><a href=k72.html>小さな恋のうた 粉ミルク<br><img src=http://img.youtube.com/vi//0.jpg></a>");
-document.write("<br><a href=k73.html>たぶん YOASOBI<br><img src=http://img.youtube.com/vi//0.jpg></a>");
-document.write("<br><a href=k74.html>涙そうそう 夏川りみ<br><img src=http://img.youtube.com/vi//0.jpg></a>");
-document.write("<br><a href=k75.html>カワキヲアメク 美波<br><img src=http://img.youtube.com/vi//0.jpg></a>");
-
-document.write("<br><a href=k76.html>Ref:rain Aimer あいみょん<br><img src=http://img.youtube.com/vi//0.jpg></a>");
-document.write("<br><a href=k77.html>ようかい体操第一 Dream5<br><img src=http://img.youtube.com/vi//0.jpg></a>");
-document.write("<br><a href=k78.html>ハッピーエンド back number<br><img src=http://img.youtube.com/vi//0.jpg></a>");
-document.write("<br><a href=k79.html>残酷な天使のテーゼ 高橋洋子<br><img src=http://img.youtube.com/vi//0.jpg></a>");
-document.write("<br><a href=k80.html>竈門炭治郎のうた 椎名豪 中川奈美<br><img src=http://img.youtube.com/vi//0.jpg></a>");
-document.write("<br><a href=k81.html>小さな恋のうた MONGOL800<br><img src=http://img.youtube.com/vi//0.jpg></a>");
-document.write("<br><a href=k82.html>名前を呼ぶよ ラックライフ<br><img src=http://img.youtube.com/vi//0.jpg></a>");
-document.write("<br><a href=k83.html>HANABI Mr.Children<br><img src=http://img.youtube.com/vi//0.jpg></a>");
-document.write("<br><a href=k84.html>恋音と雨空 AAA<br><img src=http://img.youtube.com/vi//0.jpg></a>");
-document.write("<br><a href=k85.html>明日晴れるかな 桑田佳祐<br><img src=http://img.youtube.com/vi//0.jpg></a>");
-document.write("<br><a href=k86.html>うっせぇわ Ado<br><img src=http://img.youtube.com/vi//0.jpg></a>");
-
-document.write("<br><a href=k87.html>	生きていたんだよな Aimer あいみょん<br><img src=http://img.youtube.com/vi//0.jpg></a>");
-document.write("<br><a href=k88.html>スパークル RADWIMPS<br><img src=http://img.youtube.com/vi//0.jpg></a>");
-document.write("<br><a href=k89.html>THERE IS A REASON 鈴木このみ<br><img src=http://img.youtube.com/vi//0.jpg></a>");
-document.write("<br><a href=k90.html>花に亡霊 ヨルシカ<br><img src=http://img.youtube.com/vi//0.jpg></a>");
-document.write("<br><a href=k91.html><br><img src=http://img.youtube.com/vi//0.jpg></a>");
-
-document.write("<br><a href=k92.html><br><img src=http://img.youtube.com/vi//0.jpg></a>");
-document.write("<br><a href=k93.html><br><img src=http://img.youtube.com/vi//0.jpg></a>");
-document.write("<br><a href=k94.html><br><img src=http://img.youtube.com/vi//0.jpg></a>");
-document.write("<br><a href=k95.html><br><img src=http://img.youtube.com/vi//0.jpg></a>");
 document.write("<br><a href=k96.html><br><img src=http://img.youtube.com/vi//0.jpg></a>");
 
 document.write("<br><a href=k97.html><br><img src=http://img.youtube.com/vi//0.jpg></a>");
