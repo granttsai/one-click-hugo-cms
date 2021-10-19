11afe19ee2d712ac35f5090fb8ae8d9b6c75ded4
granttsai <grantjlt@gmail.com>
2021-10-19 11:44:27 +0800

    Update youtube7.js

--- site/static/img/youtube7.js
+++ site/static/img/youtube7.js
@@ -5,7 +5,7 @@ arr.push("<br><a href=h3.html>只花20小時學會任何事情<br><img src=https
 arr.push("<br><a href=h4.html>如何讓發音更像是美式母語般發音<br><img src=https://img.youtube.com/vi/m3g51xfopIE/0.jpg></a>");
 arr.push("<br><a href=h5.html>There's more to life than being happy<br><img src=https://img.youtube.com/vi/y9Trdafp83U/0.jpg></a>");
 arr.push("<br><a href=h6.html>Is Social Media Hurting Your Mental Health?<br><img src=https://img.youtube.com/vi/Czg_9C7gw0o/0.jpg></a>");
-arr.push("<br><a href=h7.html>I Am Not Your Asian Stereotype<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=h7.html>I Am Not Your Asian Stereotype<br><img src=https://img.youtube.com/vi/_pUtz75lNaw/0.jpg></a>");
 //arr.push("<br><a href=h.html><br><img src=https://img.youtube.com/vi//0.jpg></a>");
 //arr.push("<br><a href=h.html><br><img src=https://img.youtube.com/vi//0.jpg></a>");
 
