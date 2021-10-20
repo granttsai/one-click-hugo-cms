6c7b268a0d69aa5a2bc16a6c36932774096bfe41
granttsai <grantjlt@gmail.com>
2021-10-20 09:51:23 +0800

    Update youtube0.js

--- site/static/img/youtube0.js
+++ site/static/img/youtube0.js
@@ -1,2 +1,4 @@
-document.write(a);
+var obj = document.getElementById("imgid");
+var imgSrc = obj.getAttribute("src");
+document.write(imgSrc.substr(27,11));
 document.write("<link rel='stylesheet' href='https://toshit2.netlify.app/img/youtube.css'><div class='video-container'><iframe id='player' src='https://www.youtube.com/embed/"+a.substr(252, 11)+"?enablejsapi=1' frameborder='0' controls='0' allowfullscreen></iframe></div><div class='youtube-transcript'><div id='youtube-transcript-#1' class='youtube-transcript'></div>");</script>
