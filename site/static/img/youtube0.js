ae1f9f5a506c4ad23f359b85395b47dd5f75b337
granttsai <grantjlt@gmail.com>
2021-10-20 09:46:58 +0800

    Create youtube0.js

---
+++ site/static/img/youtube0.js
@@ -0,0 +1,2 @@
+document.write(a);
+document.write("<link rel='stylesheet' href='https://toshit2.netlify.app/img/youtube.css'><div class='video-container'><iframe id='player' src='https://www.youtube.com/embed/"+a.substr(252, 11)+"?enablejsapi=1' frameborder='0' controls='0' allowfullscreen></iframe></div><div class='youtube-transcript'><div id='youtube-transcript-#1' class='youtube-transcript'></div>");</script>
