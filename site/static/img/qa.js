c607b1fec4eb6bea1465c6ff1312e6afcab0eba7
granttsai <grantjlt@gmail.com>
2021-10-21 17:24:06 +0800

    Update qa.js

--- site/static/img/qa.js
+++ site/static/img/qa.js
@@ -1,12 +1,6 @@
 var script = document.createElement('script');
           
         script.src = 
-"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js";
+"https://cpwebassets.codepen.io/assets/common/stopExecutionOnTimeout-1b93190375e9ccc259df3a57c1abc0e64599724ae30d7ea4c6877eb615f89387.js";
           
         document.head.appendChild(script)
-var script2 = document.createElement('script');
-          
-        script2.src = 
-"<script src="https://cpwebassets.codepen.io/assets/common/stopExecutionOnTimeout-1b93190375e9ccc259df3a57c1abc0e64599724ae30d7ea4c6877eb615f89387.js"></script>";
-          
-        document.head.appendChild(script2)
