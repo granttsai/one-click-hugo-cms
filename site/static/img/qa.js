ad7f43d06e2c4ab04ab4bc7b33d044d131df9528
granttsai <grantjlt@gmail.com>
2021-10-21 17:19:23 +0800

    Create qa.js

---
+++ site/static/img/qa.js
@@ -0,0 +1,6 @@
+var script = document.createElement('script');
+          
+        script.src = 
+"https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js";
+          
+        document.head.appendChild(script)
