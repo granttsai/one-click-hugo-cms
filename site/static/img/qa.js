94a5d4f03ef45b70c88295844097fe2893a4447d
granttsai <grantjlt@gmail.com>
2021-10-21 17:21:03 +0800

    Update qa.js

--- site/static/img/qa.js
+++ site/static/img/qa.js
@@ -4,3 +4,9 @@ var script = document.createElement('script');
 "https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js";
           
         document.head.appendChild(script)
+var script2 = document.createElement('script');
+          
+        script2.src = 
+"<script src="https://cpwebassets.codepen.io/assets/common/stopExecutionOnTimeout-1b93190375e9ccc259df3a57c1abc0e64599724ae30d7ea4c6877eb615f89387.js"></script>";
+          
+        document.head.appendChild(script2)
