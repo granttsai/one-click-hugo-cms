a1430e606392413e35ff5c5c7a2eea0d93272927
granttsai <grantjlt@gmail.com>
2021-10-23 17:54:47 +0900

    Update jp.js

--- site/static/img/jp.js
+++ site/static/img/jp.js
@@ -1,1 +1,1 @@
-document.write("<a href=https://jp.hiyawu.com/p/k.html><img src=http://img.youtube.com/vi/kzZ6KXDM1RI/0.jpg alt='K歌學日文'></a>");
+document.write("<a href=https://jp.hiyawu.com/p/k.html><img src=https://img.youtube.com/vi/kzZ6KXDM1RI/0.jpg alt='K歌學日文'></a>");
