8e71b738d283dba976b10497ea90769572c68366
granttsai <grantjlt@gmail.com>
2021-10-23 18:07:58 +0900

    Update youtube6.js

--- site/static/img/youtube6.js
+++ site/static/img/youtube6.js
@@ -1,19 +1,19 @@
 
 var arr = [];
 
-arr.push("<br><a href=s4.html>Olivia Rodrigo Driver License<br><img src=http://img.youtube.com/vi/ZmDBbnmKpqQ/0.jpg></a>");
-arr.push("<br><a href=s2.html>BTS防彈少年團Butter<br><img src=http://img.youtube.com/vi/WMweEpGlu_U/0.jpg></a>");
-arr.push("<br><a href=s3.html>BTS防彈少年團Permission to Dance<br><img src=http://img.youtube.com/vi/CuklIb9d3fI/0.jpg></a>");
-arr.push("<br><a href=s5.html>The Beatles - Here Comes The Sun<br><img src=http://img.youtube.com/vi/KQetemT1sWc/0.jpg></a>");
-arr.push("<br><a href=s6.html>Maroon 5 - Girls Like You<br><img src=http://img.youtube.com/vi/aJOTlE1K90k/0.jpg></a>");
-arr.push("<br><a href=s7.html>passenger let her go<br><img src=http://img.youtube.com/vi/RBumgq5yVrA/0.jpg></a>");
+arr.push("<br><a href=s4.html>Olivia Rodrigo Driver License<br><img src=https://img.youtube.com/vi/ZmDBbnmKpqQ/0.jpg></a>");
+arr.push("<br><a href=s2.html>BTS防彈少年團Butter<br><img src=https://img.youtube.com/vi/WMweEpGlu_U/0.jpg></a>");
+arr.push("<br><a href=s3.html>BTS防彈少年團Permission to Dance<br><img src=https://img.youtube.com/vi/CuklIb9d3fI/0.jpg></a>");
+arr.push("<br><a href=s5.html>The Beatles - Here Comes The Sun<br><img src=https://img.youtube.com/vi/KQetemT1sWc/0.jpg></a>");
+arr.push("<br><a href=s6.html>Maroon 5 - Girls Like You<br><img src=https://img.youtube.com/vi/aJOTlE1K90k/0.jpg></a>");
+arr.push("<br><a href=s7.html>passenger let her go<br><img src=https://img.youtube.com/vi/RBumgq5yVrA/0.jpg></a>");
 
-arr.push("<br><a href=s8.html>Ed Sheeran - Perfect<br><img src=http://img.youtube.com/vi/2Vv-BfVoq4g/0.jpg></a>");
+arr.push("<br><a href=s8.html>Ed Sheeran - Perfect<br><img src=https://img.youtube.com/vi/2Vv-BfVoq4g/0.jpg></a>");
 
-arr.push("<br><a href=s9.html>Sia - Chandelier<br><img src=http://img.youtube.com/vi/2vjPBrBU-TM/0.jpg></a>");
+arr.push("<br><a href=s9.html>Sia - Chandelier<br><img src=https://img.youtube.com/vi/2vjPBrBU-TM/0.jpg></a>");
 
-arr.push("<br><a href=s10.html>Imagine Dragons - Believer<br><img src=http://img.youtube.com/vi/7wtfhZwyrcc/0.jpg></a>");
-arr.push("<br><a href=s11.html>Rihanna - Diamonds<br><img src=http://img.youtube.com/vi/lWA2pjMjpBs/0.jpg></a>");
+arr.push("<br><a href=s10.html>Imagine Dragons - Believer<br><img src=https://img.youtube.com/vi/7wtfhZwyrcc/0.jpg></a>");
+arr.push("<br><a href=s11.html>Rihanna - Diamonds<br><img src=https://img.youtube.com/vi/lWA2pjMjpBs/0.jpg></a>");
 
   var rrr=[];
 
