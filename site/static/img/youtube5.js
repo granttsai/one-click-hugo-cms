cf5ba48615203ba58127602790ac9e00ed12d4ff
granttsai <grantjlt@gmail.com>
2021-10-25 16:24:13 +0800

    Update youtube5.js

--- site/static/img/youtube5.js
+++ site/static/img/youtube5.js
@@ -1,33 +1,25 @@
 var arr = [];
 
 arr.push("<br><a href=s3.html>IU(아이유)  Celebrity<br><img src=https://img.youtube.com/vi/0-q1KafFCLU/0.jpg></a>");
-
 arr.push("<br><a href=s2.html>Red Velvet 레드벨벳 Queendom<br><img src=https://img.youtube.com/vi/c9RzZpV460k/0.jpg></a>");
-
-
 arr.push("<br><a href=s7.html>aespa 에스파 Savage<br><img src=https://img.youtube.com/vi/WPdWvnAAurg/0.jpg></a>");
-
 arr.push("<br><a href=s4.html>KEY 키 BAD LOVE<br><img src=https://img.youtube.com/vi/OYWWnd9ACMI/0.jpg></a>");
-
 arr.push("<br><a href=s6.html>NCT 127 엔시티 127 Sticker<br><img src=https://img.youtube.com/vi/1oYWnbTSang/0.jpg></a>");
-
 arr.push("<br><a href=s8.html>SUNGMIN 성민 Goodnight Summer<br><img src=https://img.youtube.com/vi/xSXslki5xIY/0.jpg></a>");
-
 arr.push("<br><a href=s9.html>KANGTA 강타 아마 Maybe<br><img src=https://img.youtube.com/vi/TuAWforbaic/0.jpg></a>");
-
 arr.push("<br><a href=s10.html>SUNGMIN 성민 설렘꽃 Blooming<br><img src=https://img.youtube.com/vi/8sY7uUMkFdo/0.jpg></a>");
-
 arr.push("<br><a href=s11.html>HYO 효연 Second (Feat. 비비 (BIBI))'<br><img src=https://img.youtube.com/vi/rthppvDJ2fA/0.jpg></a>");
-
 arr.push("<br><a href=s12.html>KANGTA 강타 7월의 크리스마스 (Christmas in July)<br><img src=https://img.youtube.com/vi/omRMIR9sQHQ/0.jpg></a>");
-
 arr.push("<br><a href=s13.html>D.O. 디오 Rose<br><img src=https://img.youtube.com/vi/_btxV8tJM7w/0.jpg></a>");
-
 arr.push("<br><a href=s14.html>aespa 에스파 Next Level<br><img src=https://img.youtube.com/vi/4TWR90KJl84/0.jpg></a>");
-
 arr.push("<br><a href=s15.html>NCT 127 gimme gimme<br><img src=https://img.youtube.com/vi/6ZUPsl0EVuk/0.jpg></a>");
-
 arr.push("<br><a href=s16.html>BAEKHYUN ベクヒョン Get You Alone<br><img src=https://img.youtube.com/vi/uqcZUIUb7fk/0.jpg></a>");
+arr.push("<br><a href=s17.html>BTS - Spring Day 봄날<br><img src=https://img.youtube.com/vi/pjqUCtVHf9k/0.jpg></a>");
+arr.push("<br><a href=s18.html>GRAY (그레이) Make Love<br><img src=https://img.youtube.com/vi/VvZvcLDgN2s/0.jpg></a>");
+arr.push("<br><a href=s19.html>유겸 (YUGYEOM) Running Through The Rain<br><img src=https://img.youtube.com/vi/V4R2EiLggIg/0.jpg></a>");
+arr.push("<br><a href=s20.html>백아연(Baek A Yeon)의 킬링보이스 <br><img src=https://img.youtube.com/vi/Vesf60s1Srg/0.jpg></a>");
+arr.push("<br><a href=s21.html>마마무(MAMAMOO)의 킬링보이스를 라이브로!<br><img src=https://img.youtube.com/vi/WvkTcNQsxzc/0.jpg></a>");
+//arr.push("<br><a href=s1.html><br><img src=https://img.youtube.com/vi//0.jpg></a>");
   var rrr=[];
 
 my_ran4(arr.length,1,arr.length);
