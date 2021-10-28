e4b7e2a96f449c9fd81a9fba805749e9b4fc60ec
granttsai <grantjlt@gmail.com>
2021-10-28 16:25:55 +0800

    Create c.js

---
+++ site/static/img/c.js
@@ -0,0 +1,80 @@
+
+var arr = [];
+
+arr.push("<br><a href=c1.html>L1<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c2.html>L2<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c3.html>L3<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c4.html>L4<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c5.html>L5<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c6.html>L6<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c7.html>L7<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c8.html>L8<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c9.html>L9<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c10.html>L10<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c11.html>L11<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c12.html>L12<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c13.html>L13<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c14.html>L14<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c15.html>L15<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c16.html>L16<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c17.html>L17<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c18.html>L18<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c19.html>L19<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c20.html>L20<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c21.html>L21<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c22.html>L22<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c23.html>L23<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c24.html>L24<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c25.html>L25<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c26.html>L26<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c27.html>L27<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c28.html>L28<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c29.html>L29<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c30.html>L30<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c31.html>L31<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c32.html>L32<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c33.html>L33<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c34.html>L34<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c35.html>L35<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c36.html>L36<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c37.html>L37<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c38.html>L38<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c39.html>L39<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c40.html>L40<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c41.html>L41<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c42.html>L42<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c43.html>L43<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c44.html>L44<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c45.html>L45<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c46.html>L46<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c47.html>L47<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c48.html>L48<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c49.html>L49<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=c50.html>L50<br><img src=https://img.youtube.com/vi//0.jpg></a>");
+//arr.push("<br><a href=k13.html><br><img src=https://img.youtube.com/vi//0.jpg></a>");
+
+  var rrr=[];
+
+my_ran4(arr.length,1,arr.length);
+function my_ran4(n,min,max){
+  var arr=[];
+  var arr2=[];
+  for(i=0;i<max-min+1;i++){
+    arr[i]=i+min;
+  }
+  for(i=0;i<n;i++){
+    var x=parseInt(Math.random()*arr.length);
+    arr2[i]=arr[x];
+    for(j=x;j<arr.length;j++){
+      arr[j]=arr[j+1];
+    }
+    arr.length=arr.length-1;
+  }
+  rrr=arr2;
+}
+
+
+for (let i = 0; i < arr.length; i++) {
+if (arr[rrr[i]] === undefined) {}else{
+  document.write(arr[rrr[i]]);}
+}
