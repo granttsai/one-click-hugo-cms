98e0e54a32e38a91d892a2b5eb6167e099628454
granttsai <grantjlt@gmail.com>
2021-10-29 10:15:02 +0800

    Update youtube5.js

--- site/static/img/youtube5.js
+++ site/static/img/youtube5.js
@@ -19,7 +19,23 @@ arr.push("<br><a href=s18.html>GRAY (그레이) Make Love<br><img src=https://im
 arr.push("<br><a href=s19.html>유겸 (YUGYEOM) Running Through The Rain<br><img src=https://img.youtube.com/vi/V4R2EiLggIg/0.jpg></a>");
 arr.push("<br><a href=s20.html>백아연(Baek A Yeon)의 킬링보이스 <br><img src=https://img.youtube.com/vi/Vesf60s1Srg/0.jpg></a>");
 arr.push("<br><a href=s21.html>마마무(MAMAMOO)의 킬링보이스를 라이브로!<br><img src=https://img.youtube.com/vi/WvkTcNQsxzc/0.jpg></a>");
-//arr.push("<br><a href=s1.html><br><img src=https://img.youtube.com/vi//0.jpg></a>");
+arr.push("<br><a href=s22.html>N.Flying (엔플라잉) 'Sober'<br><img src=https://img.youtube.com/vi/qRi8JRpm2Mw/0.jpg></a>");
+arr.push("<br><a href=s23.html>CNBLUE (씨엔블루) – 싹둑 (Love Cut)<br><img src=https://img.youtube.com/vi/Hx4fG2Y4TWk/0.jpg></a>");
+arr.push("<br><a href=s24.html>CNBLUE (씨엔블루) – 과거 현재 미래 (Then, Now and Forever)<br><img src=https://img.youtube.com/vi/236ddk-0sOI/0.jpg></a>");
+arr.push("<br><a href=s25.html>TXT (투모로우바이투게더) 'Frost'<br><img src=https://img.youtube.com/vi/X3lA4EeeXtM/0.jpg></a>");
+arr.push("<br><a href=s26.html>TXT (투모로우바이투게더) '0X1=LOVESONG (I Know I Love You) feat. MOD SUN'<br><img src=https://img.youtube.com/vi/TP7FtzBuT8s/0.jpg></a>");
+arr.push("<br><a href=s27.html>TXT (투모로우바이투게더) 'LO$ER=LO♡ER'<br><img src=https://img.youtube.com/vi/JzODRUBBXpc/0.jpg></a>");
+arr.push("<br><a href=s28.html>TXT (투모로우바이투게더) 'Magic'<br><img src=https://img.youtube.com/vi/FQRnJvbLTAo/0.jpg></a>");
+arr.push("<br><a href=s29.html>TXT (투모로우바이투게더) '0X1=LOVESONG (I Know I Love You) feat. Seori'<br><img src=https://img.youtube.com/vi/MOKWoY8qy4E/0.jpg></a>");
+arr.push("<br><a href=s30.html>SEVENTEEN (세븐틴) 'Rock with you'<br><img src=https://img.youtube.com/vi/WpuatuzSDK4/0.jpg></a>");
+arr.push("<br><a href=s31.html>SEVENTEEN (세븐틴) 'Ready to love'<br><img src=https://img.youtube.com/vi/yCvSR4lSqTg/0.jpg></a>");
+arr.push("<br><a href=s32.html>WONWOO X MINGYU 'Bittersweet (feat. LeeHi)<br><img src=https://img.youtube.com/vi/gvdACvfuGFA/0.jpg></a>");
+arr.push("<br><a href=s33.html>NU'EST (뉴이스트) 'INSIDE OUT'<br><img src=https://img.youtube.com/vi/AIyarEsQvAg/0.jpg></a>");
+arr.push("<br><a href=s34.html>ENHYPEN (엔하이픈) 'Tamed-Dashed'<br><img src=https://img.youtube.com/vi/YsSJBNQJVT0/0.jpg></a>");
+arr.push("<br><a href=s35.html>ENHYPEN (엔하이픈) 'Tamed-Dashed' <br><img src=https://img.youtube.com/vi/6IRsW_TkyVA/0.jpg></a>");
+arr.push("<br><a href=s36.html>NU'EST - I'm in Trouble<br><img src=https://img.youtube.com/vi/4Lm9u-iXrDc/0.jpg></a>");
+
+//arr.push("<br><a href=s3.html><br><img src=https://img.youtube.com/vi//0.jpg></a>");
   var rrr=[];
 
 my_ran4(arr.length,1,arr.length);
