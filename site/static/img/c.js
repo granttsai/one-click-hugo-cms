21ab07a7bd6dda933113a8e25adc8ffd6df4ead3
granttsai <grantjlt@gmail.com>
2023-06-20 16:25:42 +0800

    Update c.js

--- site/static/img/c.js
+++ site/static/img/c.js
@@ -1,5 +1,51 @@
+document.write("<a href=c1.html>第1課</a><br>");
+document.write("<a href=c2.html>第2課</a><br>");
+document.write("<a href=c3.html>第3課</a><br>");
+document.write("<a href=c4.html>第4課</a><br>");
+document.write("<a href=c5.html>第5課</a><br>");
+document.write("<a href=c6.html>第6課</a><br>");
+document.write("<a href=c7.html>第7課</a><br>");
+document.write("<a href=c8.html>第8課</a><br>");
+document.write("<a href=c9.html>第9課</a><br>");
+document.write("<a href=c10.html>第10課</a><br>");
+document.write("<a href=c11.html>第11課</a><br>");
+document.write("<a href=c12.html>第12課</a><br>");
+document.write("<a href=c13.html>第13課</a><br>");
+document.write("<a href=c14.html>第14課</a><br>");
+document.write("<a href=c15.html>第15課</a><br>");
+document.write("<a href=c16.html>第16課</a><br>");
+document.write("<a href=c17.html>第17課</a><br>");
+document.write("<a href=c18.html>第18課</a><br>");
+document.write("<a href=c19.html>第19課</a><br>");
+document.write("<a href=c20.html>第20課</a><br>");
+document.write("<a href=c21.html>第21課</a><br>");
+document.write("<a href=c22.html>第22課</a><br>");
+document.write("<a href=c23.html>第23課</a><br>");
+document.write("<a href=c24.html>第24課</a><br>");
+document.write("<a href=c25.html>第25課</a><br>");
+document.write("<a href=c26.html>第26課</a><br>");
+document.write("<a href=c27.html>第27課</a><br>");
+document.write("<a href=c28.html>第28課</a><br>");
+document.write("<a href=c29.html>第29課</a><br>");
+document.write("<a href=c30.html>第30課</a><br>");
+document.write("<a href=c31.html>第31課</a><br>");
+document.write("<a href=c32.html>第32課</a><br>");
+document.write("<a href=c33.html>第33課</a><br>");
+document.write("<a href=c34.html>第34課</a><br>");
+document.write("<a href=c35.html>第35課</a><br>");
+document.write("<a href=c36.html>第36課</a><br>");
+document.write("<a href=c37.html>第37課</a><br>");
+document.write("<a href=c38.html>第38課</a><br>");
+document.write("<a href=c39.html>第39課</a><br>");
+document.write("<a href=c40.html>第40課</a><br>");
+document.write("<a href=c41.html>第41課</a><br>");
+document.write("<a href=c42.html>第42課</a><br>");
+document.write("<a href=c43.html>第43課</a><br>");
+document.write("<a href=c44.html>第44課</a><br>");
+document.write("<a href=c45.html>第45課</a><br>");
+document.write("<a href=c46.html>第46課</a><br>");
+document.write("<a href=c47.html>第47課</a><br>");
+document.write("<a href=c48.html>第48課</a><br>");
+document.write("<a href=c49.html>第49課</a><br>");
+document.write("<a href=c50.html>第50課</a><br>");
 
-for (let i = 1; i <=50; i++) {
-
-  document.write("<a href=c"+i+".html>第"+i+"課</a><br>");
-}
