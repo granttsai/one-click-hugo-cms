b295a5b77f1f97f05211d5e1476a470cd65c0f6f
granttsai <grantjlt@gmail.com>
2023-07-17 13:45:40 +0800

    Update jp.js

--- site/static/img/jp.js
+++ site/static/img/jp.js
@@ -1,1 +1,1 @@
-document.write("<a href=https://jp.hiyawu.com/p/k.html><img src=https://img.youtube.com/vi/kzZ6KXDM1RI/0.jpg alt='K歌學日文'></a>");
+document.write("<a href=https://jp.hiyawu.com/p/k.html><img src=https://img.youtube.com/vi/kzZ6KXDM1RI/0.jpg alt='一起K歌學日文'></a>");
