f805306d2ab39d6f3a662b7b6c7ee5540cecd145
granttsai <grantjlt@gmail.com>
2023-07-17 13:37:29 +0800

    Update c.js

--- site/static/img/c.js
+++ site/static/img/c.js
@@ -1,4 +1,4 @@
-document.write("<a href=c1.html>第1課</a><br>");
+document.write("<a href=c1.html>第1課 基礎教學 肯定、否定、疑問、也</a><br>");
 document.write("<a href=c2.html>第2課</a><br>");
 document.write("<a href=c3.html>第3課</a><br>");
 document.write("<a href=c4.html>第4課</a><br>");
